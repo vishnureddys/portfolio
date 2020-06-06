361e78c53dab20e6482e872b72c43773fa1c9e05
vishnureddys <vishnusreddy007@gmail.com>
2020-06-07 00:48:56 +0530

    Updates

---
+++ precache-manifest.0533769ee4b017a22ef5b53d96858246.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "577c35c5ad1a4cbc2371",
+    "url": "/Portfolio/static/css/main.3e8ad5c8.chunk.css"
+  },
+  {
+    "revision": "577c35c5ad1a4cbc2371",
+    "url": "/Portfolio/static/js/main.577c35c5.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/Portfolio/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "3b96116072eb85f5a3a2",
+    "url": "/Portfolio/static/js/runtime~main.3b961160.js"
+  },
+  {
+    "revision": "6c32a6e61b1bdf9d7b305cfa85be65b2",
+    "url": "/Portfolio/index.html"
+  }
+];
